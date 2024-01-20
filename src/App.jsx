ff1f98d88fdfb7e6dabd05e3d5eed00e510c326c
Naimul07 <naimul.cse.brur@gmail.com>
2024-01-20 23:41:10 +0600

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,32 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTodos([
-      ...todos, { id: crypto.randomUUID(), title: newItem, completed: false },
-    ]);
+    
+    setTodos((currentTodos)=>{
+      return [
+        ...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    });
     setNewItem("");
+  }
+  
+
+  function toggletodo(id,completed) {
+    setTodos((currentTodos)=>{
+      return currentTodos.map(todo=>{
+        if(todo.id === id)
+        {
+          return {...todo, completed}
+        }
+      })
+    })
+  }
+
+  function deleteButton(id)
+  {
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo=> todo.id !== id)
+    })
   }
 
   console.log(todos);
@@ -23,24 +45,49 @@ function App() {
             <input type="search" className="form-control" value={newItem} onChange={e => setNewItem(e.target.value)} />
           </div>
           <div className="col-auto">
-            <button type="button" className="btn btn-info">Add</button>
+            <button className="btn btn-info">Add</button>
           </div>
         </form>
       </div>
 
       <div className="tasklist mt-4">
         <h1>Tasklist</h1>
-
+        {todos.length === 0 && "No task"}
         {todos.map(todo => {
+          if(todo.completed===false)
           return (
+            
             <div className="row mb-2" key={todo.id}>
               <div className="form-check col-auto">
-                <input className="form-check-input" type="checkbox" value="" id="" />
+                <input className="form-check-input" type="checkbox" value="{todo.completed}" id="" onClick={e => toggletodo(todo.id, e.target.checked)} />
                 <label className="form-check-label">
                  {todo.title}
                 </label>
               </div>
-              <button className="btn btn-outline-danger btn-sm col-auto">Delete</button>
+              <button className="btn btn-outline-danger btn-sm col-auto" onClick={()=> deleteButton(todo.id) }>Delete</button>
+            </div>
+
+          )
+        })}
+      </div>
+
+
+
+      <div className="tasklist mt-4">
+        <h1>Completed Tasklist</h1>
+        {/* {todos.length === 0 && "No task"} */}
+        {todos.map(todo =>{
+          if(todo.completed===true)
+          return (
+            
+            <div className="row mb-2" key={todo.id}>
+              <div className="form-check col-auto">
+                <input className="form-check-input" type="checkbox" value={todo.completed} id="" onClick={e => toggletodo(todo.id, e.target.checked)} defaultChecked={true}/>
+                <label className="form-check-label">
+                 {todo.title}
+                </label>
+              </div>
+              <button className="btn btn-outline-danger btn-sm col-auto" onClick={()=> deleteButton(todo.id) }>Delete</button>
             </div>
 
           )
